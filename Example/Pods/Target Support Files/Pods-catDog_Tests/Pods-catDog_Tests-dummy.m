c0db9fa8448e4a4b31ff25fa751ec92e3743a95a
Artur <Artur>
2021-01-14 18:11:07 -0300

    big bang

---
+++ Example/Pods/Target Support Files/Pods-catDog_Tests/Pods-catDog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_catDog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_catDog_Tests
+@end
---
+++ Example/Pods/Target Support Files/catDog/catDog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_catDog : NSObject
+@end
+@implementation PodsDummy_catDog
+@end
